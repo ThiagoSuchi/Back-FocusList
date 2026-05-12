3f71306ab0587b77240ebfa56fa2324e0ce1e82e
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-12 19:15:22 -0400

    feat(services): atualizar lógica do TaskService para utilizar DTOs e validar campos anulaveis

--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -4,37 +4,56 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FocusListApi.Services
 {
-    public class TaskService
+    public class TaskService(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
-
-        // DI
-        public TaskService(AppDbContext appDbContext) => _appDbContext = appDbContext;
+        private readonly AppDbContext _appDbContext = appDbContext;
 
         // Listar todos
-        public async Task<List<TaskModel>> getAllTasks()
+        public async Task<List<TaskModel>> GetAllTasks()
         {
             return await _appDbContext.DbTasks.ToListAsync();
         }
 
         // Criar nova tarefa
-        public async Task<TaskModel> postTask(TaskModel task)
+        public async Task<TaskModel> PostTask(TaskModelDTO task)
         {
             var taskDuplicate = await _appDbContext.DbTasks.AnyAsync(t => t.Title == task.Title);
 
             if (taskDuplicate) throw new Exception("Já existe uma tarefa com esse título.");
 
-            _appDbContext.DbTasks.Add(task);
+            var newTask = new TaskModel
+            {
+                Title = task.Title,
+                Completed = task.Completed
+            };
+
+            _appDbContext.DbTasks.Add(newTask);
             await _appDbContext.SaveChangesAsync();
-            return task;
+            return newTask;
         }
 
         // Atualizar tarefa
-        // public async Task<TaskModel> putTask(int id, TaskModel updatedTask)
-        // {
-        //     var task = await _appDbContext.DbTasks.FindAsync(id);
+        public async Task<TaskModel> PutTask(int id, TaskModelUpdatingDTO updatedTask)
+        {
+            var task = await _appDbContext.DbTasks.FindAsync(id);
+            
+            if (updatedTask.Title != null) task!.Title = updatedTask.Title;
+            
+            // Usa-se a propriedade .HasValue (para verificar se um valor foi mandado na requisição) e 
+            // .Value (para extrair o valor booleano propriamente dito).
+            if (updatedTask.Completed.HasValue) task!.Completed = updatedTask.Completed.Value;
 
+            await _appDbContext.SaveChangesAsync();
+            return task!;
+        }
 
-        // }
+        // Deletar tarefas
+        public async void DeleteTask(int id)
+        {
+            var task = await _appDbContext.DbTasks.FindAsync(id);
+
+            _appDbContext.DbTasks.Remove(task!);
+            await _appDbContext.SaveChangesAsync();
+        }
     }
 }
